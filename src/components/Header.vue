883c36c3cd695a989765f6cc01e78880ae2c59d7
Vitto95 <grimaldi.vitto@gmail.com>
2021-05-20 15:04:33 +0200

    bozza navbar generata dinamicamente

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,10 @@
       </div>
       <nav class="menu-right-header">
         <ul>
-          <li>
+          <li v-for="(link, index) in links" :key="index">
+            <a href="#">{{ link.text }}</a>
+          </li>
+          <!--  <li>
             <a href="#">Characters</a>
           </li>
           <li>
@@ -35,7 +38,7 @@
           </li>
           <li>
             <a href="#">Shop</a>
-          </li>
+          </li> -->
         </ul>
       </nav>
     </div>
@@ -45,6 +48,22 @@
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      links: [
+        { text: "Characters", url: "#", current: false },
+        { text: "Comics", url: "#", current: false },
+        { text: "Movies", url: "#", current: false },
+        { text: "TV", url: "#", current: false },
+        { text: "Games", url: "#", current: false },
+        { text: "Collectibles", url: "#", current: false },
+        { text: "Videos", url: "#", current: false },
+        { text: "Fans", url: "#", current: false },
+        { text: "News", url: "#", current: false },
+        { text: "Shop", url: "#", current: false },
+      ],
+    };
+  },
 };
 </script>
 
